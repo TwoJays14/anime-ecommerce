8b487c2139b0e729d2a3bb35785795c103a6e28c
JJatau <jjatau14@outlook.com>
2023-12-16 01:10:07 +0000

    add modal

--- app/components/AnimeModal.tsx
+++ app/components/AnimeModal.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { Prop, AnimeProp } from './AnimeCard';
 
 interface AnimeModalProps {
@@ -5,12 +6,20 @@ interface AnimeModalProps {
 }
 
 const AnimeModal = ({ anime, onClose }: AnimeModalProps) => {
-  // console.log('animeDetails', animeDetails);
+  console.log('anime', anime);
 
   return (
     <div className=" bg-slate-900 bg-opacity-40  top-0 left-1/2 z-30 transform -translate-x-1/2 h-screen w-screen fixed">
-      <div className="modal-content text-red-600 font-bold text-3xl flex justify-center items-center h-full ">
+      <div className="modal-content text-red-600 font-bold text-3xl flex flex-col justify-center items-center h-full ">
         <h2>{anime?.name || 'N/A'}</h2>
+        <Image
+          width={300}
+          height={500}
+          alt={anime?.name}
+          src={`https://shikimori.one${anime?.image.original}`}
+          className="rounded-xl object-contain"
+        />
+        <p>{anime?.description}</p>
         <button onClick={onClose}>Close</button>
       </div>
     </div>
